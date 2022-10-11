a0e8012a62ca29b61baeb8f531fb9a1c40f22f9f
GotabayaV <114685258+GotabayaV@users.noreply.github.com>
2022-10-11 11:57:20 +0530

    ToDoListFormController modified.

--- src/Controller/ToDoListFormController.java
+++ src/Controller/ToDoListFormController.java
@@ -1,4 +1,71 @@
 package Controller;
 
+import Database.DBConnection;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Objects;
+
+import static Controller.LoginFormController.loginID;
+
 public class ToDoListFormController {
+    public AnchorPane root;
+    public ListView lstTasks;
+    public Label lblWelcome;
+    public Label lblUserId;
+    public TextField txtTask;
+    public Button btnDelete;
+    public Button btnUpdate;
+    public AnchorPane rootAddNew;
+    public TextField txtNewTask;
+
+    Connection connection;
+    private String userID;
+
+    public void initialize(){
+        connection = DBConnection.getDBConnection().getConnection();
+        userID = loginID;
+        loginID = null;
+        lblUserId.setText(userID);
+        try {
+            ResultSet resultSet = connection.createStatement().executeQuery("select name from user where id = '" + userID + "';");
+            resultSet.next();
+            lblWelcome.setText("Hi " + resultSet.getString(1).substring(0,1).toUpperCase() + resultSet.getString(1).substring(1) + ", Welcome to ToDo List");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAddNewOnAction(ActionEvent actionEvent) {
+    }
+
+    public void txtNewTaskOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAddToListOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLogOutOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) root.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../View/LoginForm.fxml")))));
+        stage.setTitle("Login");
+        stage.centerOnScreen();
+    }
 }
